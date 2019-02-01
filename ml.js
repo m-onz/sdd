57672b5ea09cb5aac8c748beac8666a7d9d473e5
m-onz <stphnmnslw@gmail.com>
2019-02-01 21:32:47 +0000

    init

---
+++ ml.js
@@ -0,0 +1,3 @@
+
+var SVM = require('libsvm-js/asm')
+var Kernel = require('ml-kernel')
---
+++ train.js
@@ -0,0 +1,18 @@
+var osc = require('osc')
+
+var udpPort = new osc.UDPPort({
+    localAddress: "0.0.0.0",
+    localPort: 57111,
+    metadata: true
+});
+
+udpPort.on("message", function (oscMsg) {
+    var msg = oscMsg.args[0].value
+	.split(',')
+	.slice(3)
+	.map(function (i) { return parseFloat(i); })
+
+});
+
+udpPort.open();
+
